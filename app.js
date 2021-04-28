6d9d510f3ee6a5c3f209eda7eb318a6590be1341
ysato <56924527+ysato@users.noreply.github.com>
2021-04-28 22:25:00 +0900

    feat: impl notification

--- app.js
+++ app.js
@@ -12,35 +12,17 @@ const app = new App({
   receiver: expressReceiver
 });
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
-});
+app.event('channel_created', async ({ event, client }) => {
+  try {
+    await client.chat.postMessage({
+      channel: process.env.CHANNEL_TO_NOTIFY,
+      text: `<#${event.channel.id}>`
+    });
+  } catch (error) {
+    console.log(error);
 
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
+    throw new Error('Failed posting a message.')
+  }
 });
 
 module.exports.handler = serverlessExpress({
